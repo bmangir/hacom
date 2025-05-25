7e4be028bd8fd557b2e17c696e400cf108a39c92
bmangir <berkantmangir23@gmail.com>
2025-05-25 18:37:15 +0300

    create mongo client

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,9 @@
 import os
 
-import certifi
 from dotenv import load_dotenv
-from mongoengine import connect
 from pinecone import Pinecone
-from pymongo import MongoClient
+
+from databases.mongo.mongo_connector import MongoConnector
 
 load_dotenv()
 
@@ -24,7 +23,6 @@ MONGO_AGG_DATA_DB = os.getenv("MONGO_AGG_DATA_DB")
 MONGO_LOGS_DB = os.getenv("MONGO_LOGS_DB")
 MONGO_PRECOMPUTED_DB = os.getenv("MONGO_PRECOMPUTED_DB")
 
-
 NEON_PG_URI = os.getenv("NEON_PG_URI")
 JDBC_URL=os.getenv("JDBC_URL")
 NEON_DB_USER=os.getenv("NEON_DB_USER")
@@ -39,7 +37,22 @@ ITEM_CONTENTS_HOST = os.getenv("ITEM_CONTENTS_HOST")
 ITEM_FEATURES_HOST = os.getenv("ITEM_FEATURES_HOST")
 USER_FEATURES_HOST = os.getenv("USER_FEATURES_HOST")
 
-client = MongoClient(MONGO_URI,
-                     serverSelectionTimeoutMS=5000,
-                     tlsCAFile=certifi.where(),
-                     maxPoolSize=200)
+# Redis Cloud Configuration
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = int(os.getenv("REDIS_PORT"))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+REDIS_USERNAME = os.getenv("REDIS_USERNAME")
+
+CONFLUENT_BOOTSTRAP_SERVERS = os.getenv("CONFLUENT_BOOTSTRAP_SERVERS")
+CONFLUENT_API_KEY = os.getenv("CONFLUENT_API_KEY")
+CONFLUENT_API_SECRET = os.getenv("CONFLUENT_API_SECRET")
+CONFLUENT_INTERACTION_TOPIC = os.getenv("CONFLUENT_INTERACTION_TOPIC")
+CONFLUENT_CLIENT_ID = os.getenv("CONFLUENT_CLIENT_ID")
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+try:
+    mongo_client = MongoConnector(MONGO_URI)
+    client = mongo_client.get_client()
+except:
+    print("error to get mongo client")
