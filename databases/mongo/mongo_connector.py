ccda632ed91ab18883cac79b5a134e5783ef0ec5
bmangir <berkantmangir23@gmail.com>
2025-05-25 18:37:37 +0300

    add uri parameter to avoid import error

--- databases/mongo/mongo_connector.py
+++ databases/mongo/mongo_connector.py
@@ -2,25 +2,23 @@ import certifi
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError
 
-from backend.config import MONGO_URI
-
 
 class MongoConnector:
     _instance = None
 
-    def __new__(cls):
+    def __new__(cls, uri=None):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            cls._instance.client = MongoClient(MONGO_URI)
+            cls._instance.client = MongoClient(uri)
         return cls._instance
 
-    def __init__(self):
-        self.client = self.__create_client()
+    def __init__(self, uri=None):
+        self.client = self.__create_client(uri)
 
     @staticmethod
-    def __create_client():
+    def __create_client(uri):
         try:
-            client = MongoClient(MONGO_URI,
+            client = MongoClient(uri,
                                  serverSelectionTimeoutMS=5000,
                                  tlsCAFile=certifi.where(),
                                  maxPoolSize=200)
